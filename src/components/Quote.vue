7ad8c4064951a4d92e384e24a1f0568cec13cb4a
Marvellous Ubani <marvellousubani@gmail.com>
2017-10-30 11:19:14 +0100

    'app'

---
+++ src/components/Quote.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="col-sm-6 col-md-4 col-lg-3">
+		<div class="panel panel-default">
+			<div class="panel-body quote">
+				<slot></slot>
+			</div>
+		</div>
+		
+	</div>	
+</template>
+
+<script type="text/javascript">
+   import Quote from './Quote.vue';
+
+</script>
+
+<style>
+
+.panel-body{
+	font-family: 'Arizonia', cursive;
+	font-size:24px;
+	color:steelblue;
+}
+	
+.quote {
+	cursor:pointer;
+}
+
+.quote:hover{
+	background-color: #ffe2e2;
+}
+</style>
+
+
+
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="row">
+		<app-quote v-for="(quote, index) in quotes" @click.native="deleteQuote(index)">
+		{{quote}}</app-quote>
+   </div>	
+</template>
+
+<script type="text/javascript">
+  import Quote from './Quote.vue'
+   export default {
+		props: ['quotes'],
+		components: {
+			appQuote: Quote 
+		},
+		methods: {
+			deleteQuote(index){
+				this.$emit('quoteDeleted', index);
+			}
+		}
+	}
+</script>
+
+<style>
+
+
+</style>
+
+
+
